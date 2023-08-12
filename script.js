1b94e21aa2aa484cfbe5c131f38a52917b58730b
Montaz Meah <montaz.meah@mrm.com>
2023-08-12 17:55:09 -0400

    avengers

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 // This is a comment.
+
+
+if (9 > 8) {
+    console.log("hi there");
+}
+
+
+if ( 5 * 5 === 25) {
+    console.log("twenty-five");
+}
+
+
+if (10 > 4) {
+    console.log("correct");
+}
+
+if (10 > 11) {
+    console.log("no way");
+} else {
+    console.log("haha");
+}
+
+
+if (7 * 4 === 15) {
+    console.log("incorrect");
+} else {
+    console.log("anything");
+}
+
+
+
+if (8 > 100) {
+    console.log("something");
+} else {
+    console.log("something else");
+}
+
+
+
+
+let myNumber = 7;
+
+
+if (myNumber === 1) {
+    console.log("one");
+} else if (myNumber === 2) {
+    console.log("two");
+} else if (myNumber === 3) {
+    console.log("three");
+} else {
+    console.log("I don't know.");
+}
+
+
+
+let avenger = prompt("Which Avenger are you?");
+
+if (avenger === "Hulk") {
+    console.log("Welcome, Strongest Avenger");
+} else if (avenger === "Iron Man") {
+    console.log("Welcome, richest Avenger");
+} else {
+    console.log("You're not an Avenger!");
+}
